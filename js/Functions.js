765d51bafeb8bbc72f2c3d878a8bdb034186a61c
frezmecritus <frezmecritus@gmail.com>
2014-11-13 14:12:57 -0600

    remove parka

--- js/Functions.js
+++ js/Functions.js
@@ -1,178 +1,47 @@
 function settingFunction(){
-         alert('clicked setting buttion')        
- };
+    alert('clicked setting buttion')        
+}
 
- function socialFunction(){
-         alert('clicked social buttion')        
- };
- function addressFunction(){
-         alert('clicked address buttion')   
-         if ($('.textboxes').is(':visible')) {
-                  //$('.textboxes').hide();
-                  // do save info
-                  $(this).val('Edit');
-              }
-              else {
-                  $('.textboxes').show();
-                  $(this).val('Save');
-              }     
- };
-
-  function detailFunction(){
-         //alert('clicked detail buttion');
-         $('.b_form').slideToggle();     
-         // if ($('.textboxes').is(':visible')) {
-         //          //$('.textboxes').hide();
-         //          // do save info
-         //          $(this).val('Edit');
-         //      }
-         //      else {
-         //          $('.textboxes').show();
-         //          $(this).val('Save');
-         //      }     
- };
-
- function onDeviceReady(){
-     document.addEventListener('pause', onPause, false);
-     document.addEventListener('resume', onResume, false);
-
-     window.plugin.backgroundMode.disable();
- }
- function onResume(){
-     window.plugin.backgroundMode.disable();
- }
- function onPause(){
-     window.plugin.backgroundMode.enable();
- }
- document.addEventListener('deviceready', onDeviceReady, false);
-
-
- 
-
- function index_message() {
-     app.retrieveData( 8, function(data){
-         $.each(data, function( index, value ){
-             var snow = value['snow_depth'];
-             if (index==0) {
-                 $('#show_temperature').append("<strong>Snow right now: "  + snow + " in."+ '</strong><br/>');
-                 if (snow<2) {
-                     $('#user_notification').prepend("<p id='note'>No need to panic. Keep your car right where it is.  <a id='data_drop' onclick='showData(); return false;'>&#187</a></p>");
-                 }
-                 else if (snow >= 2) {
-                     $('#user_notification').prepend("<p id='note'>Move your car off the street before 8am. Don't forget your snow boots!   <a id='data_drop' onclick='showData(); return false;'>&#187</a></p>");
-                 }
-             }
-             else {
-                 $('#show_temperature').hide();
-                 if (index==1) {
-                     hour = "hour";
-                 } else {
-                     hour = "hours";
-                 }
-                 $('#show_temperature').append(index + " " + hour + " ago:  " + snow +  " in."+ '<br/>');
-             }
-         });
-         $('#show_temperature').append("<a id='go_back' onclick='hideData(); return false;'>&#171;</a>");
-     });
- }
-
- function check_weather() {
-     var dt = new Date();
-     var myDate = (dt.getMonth()+1) + "-" + dt.getDate();
-     var myTime = dt.getHours() + ":" + dt.getMinutes() + ":" + dt.getSeconds();
-     var myMin = dt.getHours() + ":" + dt.getMinutes();
-     var mySec = dt.getSeconds();
-
-     $('#back_run_test').text('Now: ' + myDate + " "  + myTime);
-     
-     refreshStorage();
-
-     if (storage.getLocation() == 'Evanston, IL' || storage.getLocation() == 'Skokie, IL') {
-         // evanston & skokie @ 7:00; 2inch. overnigh
-
-         if (myMin == "7:0") {
-             var data = JSON.parse(storage.getSnow()), move_car = 0;
-
-             if( data[3] > 40 ) // now, but now testing on temperature
-                 move_car = 1;
-
-             if(move_car) {
-                 if(storage.getAlarm()) {
-                     window.plugin.notification.local.add({ 
-                         message: "Snow over 2 inches! Move your car now!",
-                         sound: 'TYPE_ALARM' });
-                  } else {
-                     window.plugin.notification.local.add({ 
-                         message: "Snow over 2 inches! Move your car now!" });
-                 }
-             }
+function socialFunction(){
+    alert('clicked social buttion')        
+}
+function addressFunction(){
+    alert('clicked address buttion')   
+    if ($('.textboxes').is(':visible')) {
+             //$('.textboxes').hide();
+             // do save info
+             $(this).val('Edit');
          }
-     } else if (storage.getLocation() == 'Rogers Park, IL' || storage.getLocation() == 'Lincoln Park, IL') {
-         // only notification(11/30) @ 5pm - parking ban starts 12/1->4/1 3am~7am
-         // lincon park & rogers park (recent 4inches), push right away(45 mins); check behind 2 hours;
+         else {
+             $('.textboxes').show();
+             $(this).val('Save');
+         }     
+}
 
-         var data = JSON.parse(storage.getSnow()), move_car = 0;
+function detailFunction(){
+    $('.b_form').slideToggle();     
+}
 
-         if ( myDate == "11-30" && myMin == "17:0") {
-             alert("No parking 3AM-7AM, DEC 1 - APR 1");
-         }
 
-         if( data[4] > 2 ||  data[5] > 2 ) // now, but now testing on temperature
-             move_car = 1;
+<!--//////////////not modified old code//////////////////////-->
 
-         move_car = 0;
-         if ( move_car ) {
-             if(storage.getAlarm()) {
-                 window.plugin.notification.local.add({ 
-                     message: "Snow over 2 inches! Move your car now!",
-                     sound: 'TYPE_ALARM' });
-              } else {
-                 window.plugin.notification.local.add({ 
-                     message: "Snow over 2 inches! Move your car now!" });
-             }
-         }
-
-     } else {
-         $('#back_run_test3').text('smth wrong');
-     }
-
-     setTimeout('check_weather()',45*60*1000);
- }
-
- function refreshStorage() {
-     var data, out_strings = [];
-     var out_strings = JSON.parse( storage.getSnow() );
-     
-     app.retrieveData( 4, function(data){
-         $.each(data, function( index, value ){
-             out_strings.push( value['snow_depth'] );
-             out_strings.shift();
-         });
-         
-         storage.storeSnow( JSON.stringify(out_strings) );
-     });
- }
-
- function getLocation() {
-        //alert('step1');
-       if (navigator.geolocation) {
-         navigator.geolocation.getCurrentPosition(showLocation, ErrorHandler, options);
-       }else{
-         alert('thats the problem');
-       }
-     }
+function getLocation() {
+    if (navigator.geolocation) {
+        //navigator.geolocation.getCurrentPosition(showLocation);
+        navigator.geolocation.getCurrentPosition(function(coords){
+            alert(coords.latitude);
+        });
+    }else{
+        alert('thats the problem');
+    }
+}
 function showLocation(pos) {
-       var crd = pos.coords;
-       geo_latitude = crd.latitude;
-       geo_longtitude = crd.longitude;
-       //call function to save data to cloud
-       //SavetoCloud();
-       console.log(geo_latitude);
-       console.log(geo_longtitude);
-       //pase the location to retrieve data
-       app.retrieveData(geo_latitude,geo_longtitude);
-       ConvertLocation(geo_latitude,geo_longtitude);
-     }
+    var crd = pos.coords;
+    geo_latitude = crd.latitude;
+    geo_longtitude = crd.longitude;
+    console.log(geo_latitude);
+    console.log(geo_longtitude);
+}
 function ErrorHandler(err) {
        if(err.code == 1) {
          alert("Error: Access is denied!");
@@ -207,6 +76,5 @@ function ConvertLocation(lan,log){
               console.log("Geocoding failed: " + status);
            }
         });
+    }
 }
-}
-
--- js/cross_domain.js
+++ js/cross_domain.js
@@ -1,103 +1,96 @@
 function BuoyData  (attribute, values){
 
-           // Add object properties like this
-           this.attribute = attribute;
-           this.values = values;
-        }
+    // Add object properties like this
+    this.attribute = attribute;
+    this.values = values;
+}
     
+var generateRequest = {
+    
+    init: function (location) {
 
-    simpleAJAXLib = {
+    },
+
+    getCurrentLocation: function () {
+
+    }
+}
+
+
+var simpleAJAXLib = {
+    
+    init: function (url) {
+        this.fetchJSON(url);
+    },
+
+    fetchJSON: function (url) {
+        var root = 'https://query.yahooapis.com/v1/public/yql?q=';
+        var yql = 'select * from xml where url="' + url + '"';
+        var proxy_url = root + encodeURIComponent(yql) + '&format=json&diagnostics=false&callback=simpleAJAXLib.display';
+        document.getElementsByTagName('body')[0].appendChild(this.jsTag(proxy_url));
+    },
+
+    jsTag: function (url) {
+        var script = document.createElement('script');
+        script.setAttribute('type', 'text/javascript');
+        script.setAttribute('src', url);
+        return script;
+    },
+
+    display: function (results) {
+        var dataarray = new Array(15);
+        //Get the data out of the results 
+        var text = "";
+        var text2 = results.query.results.rss.channel.item.description;
+        var items2 = text2.split('<strong>');
+
+
+        //parse the data and load the object data to dataarray and text data to text
+        for(i = 1 ; i < items2.length;i++){
+            var temptext = (items2[i]);
+            var values=temptext.split('</strong>');
+            var attribute= values[0];
+            var value_tail_out =  values[1].split('<br />');
+            var value = value_tail_out[0].trim();
+            var new_vlue = new BuoyData(attribute, value);
+            dataarray[i-1]=new_vlue;
+            text = text + attribute + "    "+ value+ "\n";
+        }        
+
+        //writte the data to each form\
+
+        //get water temperature
+        var water_temperature;
+        var water_height;
+        var wind_speeds ;
+        var air_temperature;
+        var a ="Water Temperature:";
+        for(i=0;i<dataarray.length-1;i++){
+            if(dataarray[i].attribute==a){
+                water_temperature = dataarray[i].values;
+            }
+
+            if(dataarray[i].attribute==="Significant Wave Height:"){
+                water_height = dataarray[i].values;
+            }
+
+            if(dataarray[i].attribute==="Wind Speed:"){
+                wind_speeds = dataarray[i].values;
+            }
+
+            if(dataarray[i].attribute==="Air Temperature:"){
+                air_temperature = dataarray[i].values;
+            }
+        }
         
 
-        init: function () {
-            this.fetchJSON('http://www.ndbc.noaa.gov/data/latest_obs/45007.rss');
-        },
- 
-        fetchJSON: function (url) {
-            var root = 'https://query.yahooapis.com/v1/public/yql?q=';
-            var yql = 'select * from xml where url="' + url + '"';
-            var proxy_url = root + encodeURIComponent(yql) + '&format=json&diagnostics=false&callback=simpleAJAXLib.display';
-            document.getElementsByTagName('body')[0].appendChild(this.jsTag(proxy_url));
-        },
- 
-        jsTag: function (url) {
-            var script = document.createElement('script');
-            script.setAttribute('type', 'text/javascript');
-            script.setAttribute('src', url);
-            return script;
-        },
- 
-        display: function (results) {
-            var dataarray = new Array(15);
-            //Get the data out of the results 
-            var text = "";
-            var text2 = results.query.results.rss.channel.item.description;
-            var items2 = text2.split('<strong>');
+        $('#water_temp').text(water_temperature.substring(0, 2));
+        $('#water_height').text(water_height.substring(0, 1));
+        $('#wind_speed').text(wind_speeds.substring(0, 2));
+        $('#air_temp').text(air_temperature.substring(0, 2));
 
 
-            //parse the data and load the object data to dataarray and text data to text
-            for(i = 1 ; i < items2.length;i++){
-                var temptext = (items2[i]);
-                var values=temptext.split('</strong>');
-                var attribute= values[0];
-                var value_tail_out =  values[1].split('<br />');
-                var value = value_tail_out[0].trim();
-                var new_vlue = new BuoyData(attribute, value);
-                dataarray[i-1]=new_vlue;
-                text = text + attribute + "    "+ value+ "\n";
-            }        
+        document.getElementById('buoy_detail_data').value=text;
 
-            //writte the data to each form\
-
-            //get water temperature
-            var water_temperature;
-            var water_height;
-            var wind_speeds ;
-            var air_temperature;
-            var a ="Water Temperature:";
-            for(i=0;i<dataarray.length-1;i++){
-                if(dataarray[i].attribute==a){
-                    water_temperature = dataarray[i].values;
-                }
-
-                if(dataarray[i].attribute==="Significant Wave Height:"){
-                    water_height = dataarray[i].values;
-                }
-
-                if(dataarray[i].attribute==="Wind Speed:"){
-                    wind_speeds = dataarray[i].values;
-                }
-
-                if(dataarray[i].attribute==="Air Temperature:"){
-                    air_temperature = dataarray[i].values;
-                }
-            }
-            
-
-            $('#water_temp').text(water_temperature.substring(0, 2));
-            $('#water_height').text(water_height.substring(0, 1));
-            $('#wind_speed').text(wind_speeds.substring(0, 2));
-            $('#air_temp').text(air_temperature.substring(0, 2));
-            // document.getElementById('water_temp').text=dataarray[13].values.substring(0, 3);
-            // document.getElementById('water_height').value=dataarray[5].values;
-            // document.getElementById('wind_speed').value=dataarray[3].values;
-            // document.getElementById('air_temp').value=dataarray[11].values.substring(0, 3);
-
-           
-
-            
-
-            // for(int i=0;i<items2.length;i++){
-
-            // }
-
-
-            //write the result to the panel
-
-            document.getElementById('buoy_detail_data').value=text;
-
-
-
-
-        }
     }
+}
