7096152043a42a5af698c4f6a75ee227931d7058
frezmecritus <frezmecritus@gmail.com>
2014-11-13 18:51:08 -0600

    Geolocation
    fetch data from nearest station

--- js/Functions.js
+++ js/Functions.js
@@ -1,5 +1,6 @@
 function settingFunction(){
-    alert('clicked setting buttion')        
+    //alert('clicked setting buttion');
+    $('#wind_speed').text('80');
 }
 
 function socialFunction(){
@@ -18,66 +19,27 @@ function addressFunction(){
          }     
 }
 
-
-
-
 function detailFunction(){
     $('.b_form').slideToggle();     
 }
 
 
-<!--//////////////not modified old code//////////////////////-->
 
-function getLocation() {
+
+function getLocation (callback) {
     if (navigator.geolocation) {
-        //navigator.geolocation.getCurrentPosition(showLocation);
-        navigator.geolocation.getCurrentPosition(function(coords){
-            alert(coords.latitude);
-        });
-    }else{
-        alert('thats the problem');
-    }
-}
-function showLocation(pos) {
-    var crd = pos.coords;
-    geo_latitude = crd.latitude;
-    geo_longtitude = crd.longitude;
-    console.log(geo_latitude);
-    console.log(geo_longtitude);
-}
-function ErrorHandler(err) {
-       if(err.code == 1) {
-         alert("Error: Access is denied!");
-       }else if( err.code == 2) {
-         alert("Error: Position is unavailable!");
-       }
-     }
-function options(){
-       if(navigator.geolocation){
-             // timeout at 60000 milliseconds (60 seconds)
-             var options = {timeout:60000};
-             navigator.geolocation.getCurrentPosition(showLocation, 
-              errorHandler,
-              options);
-       }else{
-           alert("Sorry, browser does not support geolocation!");
-           }
-     }
-function ConvertLocation(lan,log){
-      var geocoder = new google.maps.Geocoder();
-      var latLng = new google.maps.LatLng(lan,log);
-
-      if (geocoder) {
-        geocoder.geocode({ 'latLng': latLng}, function (results, status) {
-           if (status == google.maps.GeocoderStatus.OK) {
-              console.log(results[0].formatted_address);
-              
-              document.getElementById('txt').placeholder=results[0].formatted_address;
-
-           }
-           else {
-              console.log("Geocoding failed: " + status);
-           }
-        });
+        navigator.geolocation.getCurrentPosition( function(position) {
+            callback({latitude: position.coords.latitude, longitude: position.coords.longitude});
+            /*alert('Latitude: '          + position.coords.latitude          + '\n' +
+                  'Longitude: '         + position.coords.longitude         + '\n' +
+                  'Altitude: '          + position.coords.altitude          + '\n' +
+                  'Accuracy: '          + position.coords.accuracy          + '\n' +
+                  'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
+                  'Heading: '           + position.coords.heading           + '\n' +
+                  'Speed: '             + position.coords.speed             + '\n' +
+                  'Timestamp: '         + position.timestamp                + '\n');*/
+      });
+    } else { 
+        alert("Geolocation is not supported by this browser.");
     }
 }
--- js/cross_domain.js
+++ js/cross_domain.js
@@ -8,25 +8,37 @@ function BuoyData  (attribute, values){
 var generateRequest = {
     
     init: function (location) {
-
+        if (!location)
+            this.getCurrentLocation();
     },
 
     getCurrentLocation: function () {
+        getLocation(function(coords){
+            alert('Searching the nearest station from ' + (coords.latitude).toFixed(2) + ', ' + (coords.longitude).toFixed(2));
+            generateRequest.action(coords);
+        });
+    },
 
+    action: function (location) {
+        var radius = 100;
+        var url = 'http://www.ndbc.noaa.gov/rss/ndbc_obs_search.php?lat=' + location.latitude + '&lon=' + location.longitude + '&radius=' + radius;
+        simpleAJAXLib.init(url,0);
     }
 }
 
 
 var simpleAJAXLib = {
     
-    init: function (url) {
-        this.fetchJSON(url);
+
+    init: function (url, station) {
+        this.fetchJSON(url, station);
     },
 
-    fetchJSON: function (url) {
+    fetchJSON: function (url, station) {
         var root = 'https://query.yahooapis.com/v1/public/yql?q=';
         var yql = 'select * from xml where url="' + url + '"';
-        var proxy_url = root + encodeURIComponent(yql) + '&format=json&diagnostics=false&callback=simpleAJAXLib.display';
+        var method = (station) ? 'showStation' : 'showNear';
+        var proxy_url = root + encodeURIComponent(yql) + '&format=json&diagnostics=false&callback=simpleAJAXLib.' + method;
         document.getElementsByTagName('body')[0].appendChild(this.jsTag(proxy_url));
     },
 
@@ -37,8 +49,18 @@ var simpleAJAXLib = {
         return script;
     },
 
-    display: function (results) {
-        var dataarray = new Array(15);
+    showNear: function (results) {
+        var items = results.query.results.rss.channel.item;
+
+        var link = items[0].link; //nearest station
+
+        var url_station = 'http://www.ndbc.noaa.gov/data/latest_obs/' + link.split('station=')[1] + '.rss';
+        simpleAJAXLib.fetchJSON(url_station, 1);
+    },
+
+    showStation: function (results) {
+        //var dataarray = new Array(15);
+        var dataarray = [];
         //Get the data out of the results 
         var text = "";
         var text2 = results.query.results.rss.channel.item.description;
@@ -48,37 +70,31 @@ var simpleAJAXLib = {
         //parse the data and load the object data to dataarray and text data to text
         for(i = 1 ; i < items2.length;i++){
             var temptext = (items2[i]);
-            var values=temptext.split('</strong>');
-            var attribute= values[0];
+            var values = temptext.split('</strong>');
+            var attribute = values[0];
             var value_tail_out =  values[1].split('<br />');
             var value = value_tail_out[0].trim();
             var new_vlue = new BuoyData(attribute, value);
-            dataarray[i-1]=new_vlue;
+            dataarray[i-1] = new_vlue;
             text = text + attribute + "    "+ value+ "\n";
-        }        
+        }
 
-        //writte the data to each form\
 
-        //get water temperature
-        var water_temperature;
-        var water_height;
-        var wind_speeds ;
-        var air_temperature;
-        var a ="Water Temperature:";
-        for(i=0;i<dataarray.length-1;i++){
-            if(dataarray[i].attribute==a){
+        //writte the data to each form
+        var water_temperature = 'x';
+        var water_height = 'x';
+        var wind_speeds = 'x';
+        var air_temperature = 'x';
+
+        for(i=0;i<dataarray.length;i++){
+
+            if(dataarray[i].attribute === "Water Temperature:"){
                 water_temperature = dataarray[i].values;
-            }
-
-            if(dataarray[i].attribute==="Significant Wave Height:"){
+            } else if(dataarray[i].attribute === "Significant Wave Height:"){
                 water_height = dataarray[i].values;
-            }
-
-            if(dataarray[i].attribute==="Wind Speed:"){
+            } else if(dataarray[i].attribute === "Wind Speed:"){
                 wind_speeds = dataarray[i].values;
-            }
-
-            if(dataarray[i].attribute==="Air Temperature:"){
+            } else if(dataarray[i].attribute === "Air Temperature:"){
                 air_temperature = dataarray[i].values;
             }
         }
@@ -89,8 +105,7 @@ var simpleAJAXLib = {
         $('#wind_speed').text(wind_speeds.substring(0, 2));
         $('#air_temp').text(air_temperature.substring(0, 2));
 
-
-        document.getElementById('buoy_detail_data').value=text;
+        $('#buoy_detail_data').val(text);
 
     }
 }
